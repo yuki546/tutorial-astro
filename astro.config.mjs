858898f6e5a31f43a4155d6f36e3e25aae9ec434
Yuki <diesunabe@gmail.com>
2024-11-10 03:16:51 +0900

    Congratulations!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://exquisite-selkie-aecdf4.netlify.app/"
+  site: "https://exquisite-selkie-aecdf4.netlify.app/",
+  integrations: [preact()]
 });
